0b079af9d7b4b4d66899d4c23f3642ebd86e0407
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-05 22:16:00 +0600

    componet add  bah div add

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-const helloworld = () => {
-    {
-        return React.createElement('p',{},'helloworld')
-    }
+// const helloworld = () => {
+//     {
+//         return React.createElement('p',{},'helloworld')
+//     }
+// }
+// const  App = () => {
+//    {
+//     return React.createElement(
+//         'h1',
+//         {style:{color:'red'}},
+//         'this createElement world',
+//         helloworld()
+//      );
+//    }
+// } 
+
+
+const HelloWorld = () => {
+    return(
+        <h1>my name is mrt</h1>
+    )
 }
-const  App = () => {
-   {
-    return React.createElement(
-        'h1',
-        {style:{color:'red'}},
-        'this createElement world',
-        helloworld()
-     );
-   }
-} 
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <HelloWorld />
+            <div>this is a fokira react app</div>
+        </React.Fragment>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(React.createElement(App));
+// root.render(React.createElement(App));
+root.render(<App />)
